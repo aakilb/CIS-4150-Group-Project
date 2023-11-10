3c785d9813f9931c12103a3eed516ad8a9cdab60
Jose <jcortez@uoguelph.ca>
2023-11-09 22:00:57 -0500

    Added basic Behave test

---
+++ mathtesting.py
@@ -0,0 +1,11 @@
+class Mathtesting:
+    @staticmethod
+    def add():
+        a = 2
+        b = 5
+        return a + b
+
+    def multiply():
+        a = 5
+        b = 10
+        return a * b
