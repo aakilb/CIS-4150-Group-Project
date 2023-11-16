640174ac53228446a6766ed3e99258f4389e5004
Melissa <melissa.chinnick@outlook.com>
2023-11-16 13:10:23 -0500

    Test Monster Initialization

--- testing_code/features/Monster.feature
+++ testing_code/features/Monster.feature
@@ -9,3 +9,7 @@ Feature: Controlling Monsters
     | chicken | Chicken | 1 | 1 | pinched |
     | guard | Guard | 2 | 2 | punched |
     | bear | Bear | 4 | 2 | slashed at |
+
+  Scenario: Initialize a special monster
+    Given give special monster chicken
+    Then the monster Chicken should have special name
--- testing_code/features/steps/step_monster.py
+++ testing_code/features/steps/step_monster.py
@@ -6,7 +6,22 @@ from dicts.monsters import give_monster
 def step_impl(context, species):
   context.monster = give_monster(species, False)
 
+@given('give special monster {species}')
+def step_impl(context, species):
+  context.monster = give_monster(species, True)
+
 @then('the monster {name} should have HP={hp:d} DMG={dmg:d} Action={action}')
 def step_impl(context, name, hp, dmg, action):
   assert isinstance(context.monster, Monster)
-  # TODO: check variable values?
+  print(context.monster.name)
+  assert context.monster.name == name
+  assert context.mosnter.hp == hp
+  assert context.mosnter.max_hp == hp
+  assert context.monster.dmg == dmg
+  assert context.monster.action == action
+
+@then('the monster {name} should have special name')
+def step_impl(context, name):
+  assert isinstance(context.monster, Monster)
+  print(context.monster.name)
+  assert context.monster.name == name
