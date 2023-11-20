deca6b8f37d7fd7d0afd129e4d6ee41a62d6df67
Melissa <melissa.chinnick@outlook.com>
2023-11-19 21:23:43 -0500

    Add Monster.attack cases

--- testing_code/features/Monster.feature
+++ testing_code/features/Monster.feature
@@ -8,8 +8,22 @@ Feature: Controlling Monsters
     | chicken | Chicken | 1 | 1 | pinched |
     | guard | Guard | 2 | 2 | punched |
     | bear | Bear | 4 | 2 | slashed at |
+    | pbag | Punching-Bag | 10 | 0 | N/A |
 
   @wip
-  Scenario: Initialize a special monster
+  Scenario: Initialize Pre-set Monster with Special Name
     Given give special monster chicken
     Then the monster Chicken should have special name
+
+  Scenario: Monster attacks an opponent
+    Given give_monster chicken
+    And an opponent pbag
+    When monster attacks opponent
+    Then opponent took 1 DMG
+
+  Scenario: Monster kills an opponent
+    Given give_monster guard
+    And an opponent chicken
+    When monster attacks opponent
+    Then opponent took 2 DMG
+    And the opponent died
--- testing_code/features/steps/step_monster.py
+++ testing_code/features/steps/step_monster.py
@@ -10,18 +10,41 @@ def step_impl(context, species):
 def step_impl(context, species):
   context.monster = give_monster(species, True)
 
+@given('an opponent {species}')
+def step_imp(context, species):
+  context.opponent = give_monster(species)
+
+@when('monster attacks opponent')
+def step_imp(context):
+  context.monster.attack(context.opponent)
+
 @then('the monster {name} should have HP={hp:d} DMG={dmg:d} Action={action}')
 def step_impl(context, name, hp, dmg, action):
   assert isinstance(context.monster, Monster)
-  print(context.monster.name)
   assert context.monster.name == name
   assert context.monster.hp == hp
   assert context.monster.max_hp == hp
   assert context.monster.dmg == dmg
-  assert context.monster.action == action
+  if action == "N/A":
+    assert context.monster.action == ""
+  else:
+    assert context.monster.action == action
+  assert context.monster.alive
+  assert context.monster.length == 0
 
 @then('the monster {name} should have special name')
 def step_impl(context, name):
   assert isinstance(context.monster, Monster)
-  print(context.monster.name)
-  assert context.monster.name == name
+  assert context.monster.name.endswith(name)
+  # TODO: Assert there is something before `name`?
+  assert context.monster.name.find(name) > 0
+
+@then('opponent took {dmg_taken:d} DMG')
+def step_impl(context, dmg_taken):
+  assert context.opponent.max_hp - context.opponent.hp == dmg_taken
+
+@then('the opponent died')
+def step_impl(context):
+  assert context.opponent.hp <= 0
+  assert context.opponent.alive == False
+  assert context.opponent.name.startswith('Dead ')
