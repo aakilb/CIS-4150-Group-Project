9e6091399880bafb0cdacb774dd015e50b7b3e94
Melissa <melissa.chinnick@outlook.com>
2023-11-19 22:22:12 -0500

    Test Player.attack on Monster

--- testing_code/features/Player.feature
+++ testing_code/features/Player.feature
@@ -10,3 +10,10 @@ Feature: Controlling the Player
   Scenario: Initialize a Player with nothing
     Given a new Player with a invalid_weapon
     Then throw a "KeyError" for invalid weapon
+
+  Scenario: Player attacks a Monster
+    Given a new Player with a bow
+    And a monster chicken
+    When the player attacks a monster
+    Then the monster took 3 DMG
+    And the player's skill increases
--- testing_code/features/steps/step_player.py
+++ testing_code/features/steps/step_player.py
@@ -1,6 +1,7 @@
 from behave import *
 # from player import Player
 from dicts.weapons_skills import *
+from dicts.monsters import give_monster
 
 @given('a new Player with a {weapon}')
 def step_impl(context, weapon):
@@ -8,6 +9,14 @@ def step_impl(context, weapon):
     context.player = Player('Bori', 10, WEAPONS[weapon])
   except Exception as e:
     context.exc = e
+
+@given('a monster {species}')
+def step_impl(context, species):
+  context.monster = give_monster(species)
+
+@when('the player attacks a monster')
+def step_impl(context):
+  context.player.attack(context.monster)
 
 @then('the player should be initialized with a {weapon}')
 def step_impl(context, weapon):
@@ -25,3 +34,11 @@ def step_impl(context, weapon):
 @then('throw a "{error}" for invalid weapon')
 def step_impl(context, error):
   assert isinstance(context.exc, eval(error))
+
+@then('the player\'s skill increases')
+def step_impl(context):
+  assert context.player.skill == 1
+
+@then('the monster took {dmg:d} DMG')
+def step_impl(context, dmg):
+  assert context.monster.max_hp - context.monster.hp == dmg
