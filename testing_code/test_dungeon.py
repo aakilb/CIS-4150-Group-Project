4319a8c28dfe2d4431f5417da9802efcab9c313e
Aakil Bohra <abohra@uoguelph.ca>
2023-11-22 20:11:56 -0500

    Updated test for Dungeon

--- testing_code/test_dungeon.py
+++ testing_code/test_dungeon.py
@@ -1,10 +1,12 @@
 from dungeon import *
 from player import *
+
 def test_dungeon():
     player = Player('TestPlayer', 50, WEAPONS[SWORD])
-    world = Dungeon(me, ROOMS)
+    world = Dungeon(player, ROOMS)
     assert Dungeon.SCREEN_WIDTH == 80
     assert world.location == 'house_63'
     assert world.current_room == ROOMS[world.location]
     assert world.rooms == ROOMS
+    assert world.player == player
     
